c613fc5d0fda534e5369e53c59f0a531d1a95cd9
Pabloin <pablo.ezequiel.inchausti@gmail.com>
2023-08-23 03:20:28 -0300

    Wip Z02a

--- glaciar/012_PEI_Models/T2023-08/GPI_Series/gpi_model_z02/xModel-set.sh
+++ glaciar/012_PEI_Models/T2023-08/GPI_Series/gpi_model_z02/xModel-set.sh
@@ -4,12 +4,8 @@ export MODEL_NAME=gpi_model_z02
 export MODEL_PATH=~/deepracer-on-the-spot-pei/glaciar/012_PEI_Models/T2023-08/GPI_Series/$MODEL_NAME
 
 
-rm ~/deepracer-on-the-spot-pei/custom-files/*.json 
-rm ~/deepracer-on-the-spot-pei/custom-files/README.md 
-rm ~/deepracer-on-the-spot-pei/custom-files/reward_function.py 
-rm ~/deepracer-on-the-spot-pei/custom-files/run.env 
-rm ~/deepracer-on-the-spot-pei/custom-files/system.env
-
+ls -la   ~/deepracer-on-the-spot-pei/custom-files
+mkdir -p ~/deepracer-on-the-spot-pei/custom-files
 
 cp ${MODEL_PATH}/hyperparameters.json   ~/deepracer-on-the-spot-pei/custom-files
 cp ${MODEL_PATH}/model_metadata.json    ~/deepracer-on-the-spot-pei/custom-files
@@ -17,6 +13,7 @@ cp ${MODEL_PATH}/reward_function.py     ~/deepracer-on-the-spot-pei/custom-files
 
 cp ${MODEL_PATH}/run.env                ~/deepracer-on-the-spot-pei/custom-files
 cp ${MODEL_PATH}/system.env             ~/deepracer-on-the-spot-pei/custom-files
+
 
 ls   -la   ~/deepracer-on-the-spot-pei/custom-files
 head -40   ~/deepracer-on-the-spot-pei/custom-files/run.env  
