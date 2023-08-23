e1f3e95edbf2b90a4477bb648748859ab341b905
Pabloin <pablo.ezequiel.inchausti@gmail.com>
2023-08-22 23:42:18 -0300

    Fixes de w02 y z01 para DOTS ...

--- glaciar/012_PEI_Models/T2023-08/GPI_Series/gpi_model_z01/xModel-set.sh
+++ glaciar/012_PEI_Models/T2023-08/GPI_Series/gpi_model_z01/xModel-set.sh
@@ -3,13 +3,8 @@
 export MODEL_NAME=gpi_model_z01
 export MODEL_PATH=~/deepracer-on-the-spot-pei/glaciar/012_PEI_Models/T2023-08/GPI_Series/$MODEL_NAME
 
-
-rm ~/deepracer-on-the-spot-pei/custom-files/*.json 
-rm ~/deepracer-on-the-spot-pei/custom-files/README.md 
-rm ~/deepracer-on-the-spot-pei/custom-files/reward_function.py 
-rm ~/deepracer-on-the-spot-pei/custom-files/run.env 
-rm ~/deepracer-on-the-spot-pei/custom-files/system.env
-
+ls -la   ~/deepracer-on-the-spot-pei/custom-files
+mkdir -p ~/deepracer-on-the-spot-pei/custom-files
 
 cp ${MODEL_PATH}/hyperparameters.json   ~/deepracer-on-the-spot-pei/custom-files
 cp ${MODEL_PATH}/model_metadata.json    ~/deepracer-on-the-spot-pei/custom-files
