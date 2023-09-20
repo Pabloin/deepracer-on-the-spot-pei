565a7eddacec359cb5fc4c2570098fb8f4896a60
Pabloin <pablo.ezequiel.inchausti@gmail.com>
2023-09-19 23:44:51 -0300

    Agrego Version Africa

---
+++ glaciar/012_PEI_Models/T2023-09/AWS_Series/AWS-Africa-A01/xModel-Readme.md
@@ -0,0 +1,15 @@
+Model The Africa Beast ... 
+
+
+       ./create-spot-instance.sh     base-africa aws-africa-a01      120  
+       ./create-standard-instance.sh base-africa aws-africa-a01      120
+
+------------------------------------------------
+
+       ./create-spot-instance.sh base-africa aws-africa-a01    120
+
+
+------------------------------------------------
+
+
+       idea .. Entrenar cada pista ... 
---
+++ glaciar/030_Scripts/.bashRegion_Africa.sh
@@ -0,0 +1,12 @@
+
+##----------------------------
+
+export MODEL_PATH=~/deepracer-on-the-spot-pei/glaciar/012_PEI_Models
+
+alias A01= "cd ${MODEL_PATH}/T2023-09/AWS_Series/AWS-Africa-A01/.config_A01"
+alias A01A="cd ${MODEL_PATH}/T2023-09/AWS_Series/AWS-Africa-A01/.config_A01a"
+alias A01B="cd ${MODEL_PATH}/T2023-09/AWS_Series/AWS-Africa-A01/.config_A01b"
+alias A01C="cd ${MODEL_PATH}/T2023-09/AWS_Series/AWS-Africa-A01/.config_A01c"
+
+##----------------------------
+
